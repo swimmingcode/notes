4b0499c0d009a4146bc7dc5c7547dd6879028a49
youjiancheng <1756730392@qq.com>
2021-03-04 16:11:21 +0800

    fix(dev)：Object添加

---
+++ javase/src/org/youyuan/test/ObjectTest.java
@@ -0,0 +1,22 @@
+package org.youyuan.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Describe: #请描述当前类的功能#
+ * @Author: youjiancheng
+ * @date 2021/3/4 16:06
+ */
+public class ObjectTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1222);
+        integers.add(56);
+        integers.add(43);
+        Object o = integers;
+        System.out.println(o);
+         ArrayList<Integer> res= (ArrayList<Integer>) o;
+        System.out.println(res);
+    }
+}
