169ede278bd434d4a1ee4c8bdd06f8d657aa1e7b
youjiancheng <1756730392@qq.com>
2021-03-29 18:11:31 +0800

    feat(websocket): websocket聊天室

---
+++ chat/src/main/java/org/youyuan/chat/chat/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package org.youyuan.chat.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+/**
+ * Created by sang on 16-12-22.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/endpointChat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue");
+    }
+
+
+
+}
--- websocket/src/main/java/org/youyuan/websocket/websocket/singlechat/WsController.java
+++ websocket/src/main/java/org/youyuan/websocket/websocket/singlechat/WsController.java
@@ -1,19 +1,34 @@
 package org.youyuan.websocket.websocket.singlechat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+import java.util.Date;
+
+/**
+ * @author YJP
+ */
 @Controller
 public class WsController {
 
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
     //@MessageMapping用来接收/welcome路径发送的消息
     @MessageMapping("/welcome")
-    //转发的路径
-    @SendTo("/topic/getResponse")
-    public ResponseMessage say(RequestMessage message) {
+    public void say(RequestMessage message) {
         System.out.println(message.getName());
-        return new ResponseMessage("welcome," + message.getName() + " !");
+        //转发的路径 群发送
+        simpMessagingTemplate.convertAndSend("/app/getResponse",new ResponseMessage("welcome," + message.getName() + " !"));
+    }
+
+    @Scheduled(cron = "0/5 * * * * *")
+    public void cronSendMessage() {
+        //转发的路径 群发送
+        simpMessagingTemplate.convertAndSend("/app/getResponse",new ResponseMessage("welcome," + new Date() + " !"));
     }
 
 
