df7a6494f009fb4940df6372c29d11ba90e05c2e
youjiancheng <1756730392@qq.com>
2021-04-08 14:19:30 +0800

    feat(elasticsearch): 搜索

---
+++ elasticsearch/src/main/java/org/youyuan/elasticsearch/bean/User.java
@@ -0,0 +1,19 @@
+package org.youyuan.elasticsearch.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Describe: #请描述当前类的功能#
+ * @Author: youjiancheng
+ * @date 2021/4/6 19:31
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
+
