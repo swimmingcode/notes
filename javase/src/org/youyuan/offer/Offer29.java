2dcacf7d859df390f436f3dff24d2019a485f671
youjiancheng <1756730392@qq.com>
2021-07-13 22:28:58 +0800

    feat(leetcode): 剑指offer29

--- javase/src/org/youyuan/offer/Offer29.java
+++ javase/src/org/youyuan/offer/Offer29.java
@@ -31,7 +31,53 @@ package org.youyuan.offer;
  */
 public class Offer29 {
 
-    public int[] spiralOrder(int[][] matrix) {
-        return null;
+    public static void main(String[] args) {
+        //[[1,11],[2,12],[3,13],[4,14],[5,15],[6,16],[7,17],[8,18],[9,19],[10,20]]
+        System.out.println(new Offer29().spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}).toString());
+//        System.out.println(new Offer29().spiralOrder(null));
     }
+
+
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
+        int[] res = new int[matrix.length * matrix[0].length];
+        int n = 0;
+        //定义左右（0，length-1）、上下(0,length-1)
+        int left = 0,right = matrix[0].length-1, top = 0 ,bottom = matrix.length-1;
+        //判断 当触碰到任何一端时  直接break
+        while (true) {
+            //从左向右
+            for (int i = left; i <= right ; i++) {
+                res[n++] = matrix[top][i];
+            }
+            if (++top > bottom) {
+                break;
+            }
+            //从上到下
+            for (int i = top; i <= bottom; i++) {
+                res[n++] = matrix[i][right];
+            }
+            if (--right < left) {
+                break;
+            }
+            //从右到左
+            for (int i = right ; i >= left ; i--) {
+                res[n++] = matrix[bottom][i];
+            }
+            if (--bottom < top) {
+                break;
+            }
+            //从下到上
+            for (int i = bottom; i >= top ; i--) {
+                res[n++] = matrix[i][left];
+            }
+            if (++left > right) {
+                break;
+            }
+        }
+        return res;
+    }
+
 }
