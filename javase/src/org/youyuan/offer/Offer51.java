a6419500d7b0f136670846cbde5425f42f787d65
youjiancheng <1756730392@qq.com>
2021-08-11 15:04:31 +0800

    test(string): 引用传递

--- javase/src/org/youyuan/offer/Offer51.java
+++ javase/src/org/youyuan/offer/Offer51.java
@@ -20,7 +20,15 @@ package org.youyuan.offer;
 public class Offer51 {
 
     public int reversePairs(int[] nums) {
-        return 0;
+        int res = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j <nums.length ; j++) {
+                if (nums[i] > nums[j]) {
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 
 }
